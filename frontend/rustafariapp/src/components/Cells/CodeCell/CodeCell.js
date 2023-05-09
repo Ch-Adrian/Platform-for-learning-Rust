d727629c0fa25bd4fbbe5b10aec253730c6c8852
Piotr Socała <piotrsocala@student.agh.edu.pl>
2023-05-09 15:09:16 +0200

    Split outputs of tests and code execution and minor improvements

--- frontend/rustafariapp/src/components/Cells/CodeCell/CodeCell.js
+++ frontend/rustafariapp/src/components/Cells/CodeCell/CodeCell.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from 'react';
+import React, { useRef, useContext, useState } from 'react';
 import Editor from '@monaco-editor/react';
 import Button from 'react-bootstrap/Button';
 import "./CodeCell.css"
@@ -10,6 +10,7 @@ import { LessonContext } from '../../../contexts/LessonContext/LessonContextProv
 const CodeCell = (props) => {
   const editorRef = useRef(null);
   const {updateCell} = useContext(LessonContext);
+  const [isExecuting, setIsExecuting] = useState(false);
 
   const handleEditorDidMount = (editor, monaco) => {
     editorRef.current = editor;
@@ -24,31 +25,31 @@ const CodeCell = (props) => {
     monaco.editor.setTheme('rustafariapp');
   }
 
-  const compile = () => {
+  const compile = async () => {
+    setIsExecuting(true);
     let modifiedCell = props.cell;
     if (props.cell.test){
-      CodeExecutorService.compileTestAndRun(editorRef.current.getValue(), props.cell.test)
+      await CodeExecutorService.compileTestAndRun(editorRef.current.getValue(), props.cell.test)
       .then((res) => {
-        modifiedCell.output = res.data;
-        modifiedCell.output += "\n";
+        modifiedCell.outputTest = res.data;
+        modifiedCell.outputTest += "\n";
       })
       .catch((err) => console.log(err));
-    } else {
-      modifiedCell.output = "";
     }
+    
 
-    CodeExecutorService.compileAndRun(editorRef.current.getValue())
+    await CodeExecutorService.compileAndRun(editorRef.current.getValue())
     .then((res) => {
-      modifiedCell.output += res.data;
+      modifiedCell.output = res.data;
       updateCell(modifiedCell, props.cellIdx, props.currentPage);
     })
     .catch((err) => console.log(err));
+    setIsExecuting(false);
   };
 
 
   return (
     <div>
-
       <Editor 
        options={{
         minimap: {
@@ -65,10 +66,10 @@ const CodeCell = (props) => {
        defaultValue={props.text} />
 
       <div className='editor-button-container'>
-       <Button onClick={compile} className='editor-button' variant="success">Run code</Button>{' '}
+       <Button onClick={compile} className='editor-button' variant="success" disabled={isExecuting}>{!isExecuting ? 'Run code' : 'Running...'}</Button>{' '}
       </div>  
-
-      {props.cell.output && <OutputCell output={props.cell.output}></OutputCell>}
+      {props.cell.outputTest && <div> Test Output <OutputCell output={props.cell.outputTest}></OutputCell></div>}
+      {props.cell.output && <div> Code output <OutputCell output={props.cell.output}></OutputCell></div>}
     </div>
   )
 }
--- frontend/rustafariapp/src/contexts/LessonContext/LessonContextProvider.js
+++ frontend/rustafariapp/src/contexts/LessonContext/LessonContextProvider.js
@@ -1,9 +1,18 @@
-import React, { createContext, useMemo, useState } from "react";
+import React, { createContext, useMemo, useState, useEffect } from "react";
 
 export const LessonContext = createContext({});
 
 const LessonContextProvider = (props) => {
-    const [lessonDefinition, setLessonDefinition] = useState({});
+    const [lessonDefinition, setLessonDefinition] = useState();
+
+    // Those two functions below are for persisiting the lesson state on page refresh
+    useEffect(() => {
+        if (window.localStorage.getItem('lessonDefinition') !== 'undefined') setLessonDefinition(JSON.parse(window.localStorage.getItem('lessonDefinition')));
+    }, []);
+
+    useEffect(() => {
+        window.localStorage.setItem('lessonDefinition', JSON.stringify(lessonDefinition));
+    }, [lessonDefinition]);
 
     const value = useMemo(() => {
         return {
@@ -13,6 +22,7 @@ const LessonContextProvider = (props) => {
     }, [lessonDefinition]);
 
     const updateCell = (newCell, idx, page) => {
+        console.log(newCell);
         let modifiedPage = {...lessonDefinition};
         modifiedPage.pages[page].cells[idx] = newCell;
         setLessonDefinition({

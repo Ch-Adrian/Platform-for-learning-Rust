9781bf6af1a84d7b72c3b94b32e13e3e19ff1d39
Ch-Adrian <aderter116@gmail.com>
2023-12-30 15:25:27 +0100

    fix: change font of text in output of cells

--- frontend/src/components/Cells/OutputCell/OutputCell.js
+++ frontend/src/components/Cells/OutputCell/OutputCell.js
@@ -11,7 +11,7 @@ const OutputCell = (props) => {
   return (
     <div className='output-container'>
       <div className='output-title'>{props.titleValue}</div>
-      <ReactMarkdown className={'markdown-output markdown-body'} remarkPlugins={[remarkGfm]}>
+      <ReactMarkdown className={'markdown-output markdown-body output-font'} remarkPlugins={[remarkGfm]}>
           {String(props.output)}
       </ReactMarkdown>
       <button  data-cy="clear-output-button" onClick={props.clearOutputHandler} className='clear-output-editor-button editor-button' variant="danger"><GrClear className="grIcon" /></button>

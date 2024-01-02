723af3329ad19d7efe9f7b3d3423c2296e1db204
Piotr Socała <piotrsocala@student.agh.edu.pl>
2024-01-02 22:02:42 +0100

    addidional adjustments

--- frontend/src/components/Cells/OutputCell/OutputCell.js
+++ frontend/src/components/Cells/OutputCell/OutputCell.js
@@ -10,11 +10,14 @@ import { GrClear } from "react-icons/gr";
 const OutputCell = (props) => {
   return (
     <div className='output-container'>
-      <div className='output-title'>{props.titleValue}</div>
+      <div className="output-misc">
+        <div className='output-title'>{props.titleValue}</div>
+        <button  data-cy="clear-output-button" onClick={props.clearOutputHandler} className='clear-output-editor-button editor-button' variant="danger"><GrClear className="grIcon" /></button>
+      </div>
       <ReactMarkdown className={'markdown-output markdown-body output-font'} remarkPlugins={[remarkGfm]}>
           {String(props.output)}
       </ReactMarkdown>
-      <button  data-cy="clear-output-button" onClick={props.clearOutputHandler} className='clear-output-editor-button editor-button' variant="danger"><GrClear className="grIcon" /></button>
+      
     </div>
   )
 }
